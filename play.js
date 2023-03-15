6828d813da2ca72710ff690033db75d86a40e3a9
Rain Zhu <rain1995zhu@gmail.com>
2023-03-15 14:52:21 -0700

    added setupInput

--- play.js
+++ play.js
@@ -1,4 +1,4 @@
-const connect = require('./client')
+const connect = require('./client');
 
 
 console.log("Connecting ...");
@@ -6,10 +6,22 @@ connect();
 
 // setup interface to handle user input from stdin
 
-// const setupInput = function () {
-//   const stdin = process.stdin;
-//   stdin.setRawMode(true);
-//   stdin.setEncoding("utf8");
-//   stdin.resume();
-//   return stdin;
-// };
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = function() {
+    // your code here
+    // \u0003 maps to ctrl+c input
+    if (key === '\u0003') {
+      process.exit();
+    }
+  };
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+
+};
