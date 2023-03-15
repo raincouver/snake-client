1da2e35ca70694aa21408dc2f70a670b2dda0c25
Rain Zhu <rain1995zhu@gmail.com>
2023-03-15 14:45:16 -0700

    leanred about movements commands

--- client.js
+++ client.js
@@ -1,44 +1,56 @@
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
-    host:'165.227.47.243',
-    port:'50541',
+    host: 'localhost',
+    port: '50541',
   });
 
-  // const client = net.createConnection(conn);
-  
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
   conn.on("connect", () => {
-  // code that does something when the connection is first established
-  
+    // code that does something when the connection is first established
+    console.log(`Successfully connected to game server`);
 
-  console.log(`Successfully connected to game server`);
-  // client.write(`setName ${conn}`);
+    // conn.write("Name: FBI");
 
-  });
-
-
-  
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
   });
 
   conn.on("connect", () => {
-    conn.write("Name: RZ");
+    // code that does something when the connection is first established
+    // console.log(`Successfully connected to game server`);
+
+    conn.write("Name: FBI");    
+    // setTimeout(() => {
+    //   conn.write("Move: down");
+    // }, 50);
+    // setTimeout(() => {
+    //   conn.write("Move: down");
+    // }, 100);
+    // setTimeout(() => {
+    //   conn.write("Move: down");
+    // }, 150);
+    // setTimeout(() => {
+    //   conn.write("Move: down");
+    // }, 200);
+    // setInterval(function(){
+    //   conn.write("Move: down");
+    //   conn.write("Move: down");
+    //   conn.write("Move: down");
+    //   conn.write("Move: down");
+    //   conn.write("Move: down");
+    //   conn.write("Move: down");
+    //   conn.write("Move: down");
+    //   conn.write("Move: down");
+    //   conn.write("Move: down");
+    //   conn.write("Move: down");
+    // }, 50); 
   });
 
-  // client.on('data', (data) => {
-  //   console.log(`${data}`);
-  // });
-
-  return conn;
 };
-
-console.log("Connecting ...");
-connect();
+// console.log("Connecting ...");
+// connect();
 
 module.exports = connect;
--- play.js
+++ play.js
@@ -1,2 +1,15 @@
 const connect = require('./client')
 
+
+console.log("Connecting ...");
+connect();
+
+// setup interface to handle user input from stdin
+
+// const setupInput = function () {
+//   const stdin = process.stdin;
+//   stdin.setRawMode(true);
+//   stdin.setEncoding("utf8");
+//   stdin.resume();
+//   return stdin;
+// };
